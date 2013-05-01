AOKP/vendor_aokp

[2/3] Install persistence support
Add persist.sh to save and restore data across installs.  Initial data
includes property ro.sf.lcd_density and file etc/hosts.  Behavior is
controlled via configuration file etc/persist.conf.

Change-Id: I946d620e06a8dc63a6abec862a7517f8b458f2d1

configs/common.mk
@@ -31,6 +31,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
     persist.sys.root_access=3
 
+# Installer
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/bin/persist.sh:install/bin/persist.sh \
+    vendor/aokp/prebuilt/common/etc/persist.conf:system/etc/persist.conf
+
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
     vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf

aways/android_vendor_crom

Conflicts:
	configs/common.mk

config/common.mk
@@ -110,6 +110,11 @@ PRODUCT_COPY_FILES += \
     vendor/crom/prebuilt/common/bin/50-crom.sh:system/addon.d/50-crom.sh \
     vendor/crom/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
+# Installer
+PRODUCT_COPY_FILES += \
+    vendor/carbon/prebuilt/common/bin/persist.sh:install/bin/persist.sh \
+    vendor/carbon/prebuilt/common/etc/persist.conf:system/etc/persist.conf
+
 # sip/voip
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml